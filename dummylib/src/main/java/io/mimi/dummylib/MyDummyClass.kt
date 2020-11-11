558fa4e6d138cb2ba97c35da5748b4bd14c65b68
Gonzalo <gonzalo@mimi.io>
2020-11-11 17:33:43 +0100

    Initial commit

---
+++ dummylib/src/main/java/io/mimi/dummylib/MyDummyClass.kt
@@ -0,0 +1,4 @@
+package io.mimi.dummylib
+
+class MyDummyClass {
+}
---
+++ dummylib/src/test/java/MyDummyClassTest.kt
@@ -0,0 +1,9 @@
+import junit.framework.Assert.assertEquals
+import org.junit.Test
+
+class MyDummyClassTest {
+    @Test
+    fun addition_isCorrect() {
+        assertEquals(4, 2 + 2)
+    }
+}
